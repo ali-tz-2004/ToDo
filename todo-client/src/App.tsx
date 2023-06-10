bf7250318def86924d77408d47236507e628ed0f
alitz <alitz1382@gmail.com>
2023-06-10 16:46:58 +0330

    add router page

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,31 +1,10 @@
-import { Container, styled } from '@mui/material';
 import './App.css';
-import { BoxContainer } from './container/BoxContainer';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-const Center = styled('div')(() => ({
-  display: "flex",
-  justifyContent: 'center',
-}));
-
-const Item = styled('div')(() => ({
-  display: "flex",
-  justifyContent: 'center',
-  width: "600px",
-  padding: '50px',
-}));
+import { Main } from './components/Main';
 
 function App() {
   return (
-    <Container fixed>
-      <Center>
-        <Item>
-          <BoxContainer />
-        </Item>
-      </Center>
-      <ToastContainer position="bottom-left" />
-    </Container>
+    <Main />
   );
 }
 
--- todo-client/src/index.tsx
+++ todo-client/src/index.tsx
@@ -3,17 +3,59 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './components/Login';
+import styled from '@emotion/styled';
+import { Container } from '@mui/material';
+import { ToastContainer } from 'react-toastify';
+import { BoxContainer } from './container/BoxContainer';
+import { Register } from './components/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/todo",
+    element: <BoxContainer />
+  },
+]);
+
+const Center = styled('div')(() => ({
+  display: "flex",
+  justifyContent: 'center',
+}));
+
+const Item = styled('div')(() => ({
+  width: "600px",
+  padding: '50px',
+  position: "relative",
+}));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Container fixed>
+      <Center>
+        <Item>
+          <RouterProvider router={router} />
+        </Item>
+      </Center>
+      <ToastContainer position="bottom-left" />
+    </Container>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
