14a201b6190470329e6f78b1ca11d9255f8d99ec
alitz <alitz1382@gmail.com>
2023-06-10 17:15:59 +0330

    add README

--- todo-service/Todo.API/Dto/TodoDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Todo.API.Dto;
-public class TodoDto
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime CompleteDate { get; set; }
-}
